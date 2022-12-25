1227c34fca3c9e853dc101061987a42252c82353
Hoang <hoangnym94@gmail.com>
2022-12-25 15:13:54 +0100

    add key in objects array function

--- practice-for-week-02-pojo-main/problems/06-key-in-object-array.js
+++ practice-for-week-02-pojo-main/problems/06-key-in-object-array.js
@@ -21,6 +21,14 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // Your code here
+  for (let i = 0; i < objArray.length; i++) {
+    for (let key in objArray[i]) {
+      if (key === keyString) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
